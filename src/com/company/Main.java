53cb344c832ce22b1b93d29d3ed0d996df9296fc
Angelo Lim <angelovinolim@gmail.com>
2018-09-30 20:54:58 -0400

    It is represented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,12 +10,16 @@ public class Main {
         System.out.println("Enter in a short word to be encrypted.");
         String decryption = kbInput.next();
         char[] decryptionArray = decryption.toCharArray();
+        String value = "";
 
-        int decryptionLength = decryption.length();
-        double number = (Math.random() + 0.2) * 5;
-        int type = (int)number;
+        int decryptionLength = 0;
+        int type = 0;
 
-        double percent = Math.random() * decryptionLength;
+            decryptionLength = decryption.length();
+            double number = (Math.random() + 0.2) * 5;
+            type = (int)number;
+
+        double percent = (Math.random() + 0.01) * decryptionLength ;
         int num;
 
         if (percent - (int)percent >= 0.50) {
@@ -24,15 +28,39 @@ public class Main {
             num = (int)Math.floor(percent);
         }
 
-        if (type == 1) {
+        int o = 0;
 
-        } else if (type == 2) {
+        for (int i = 0; i < decryptionLength; i++) {
+            char character = decryption.charAt(num + o);
+            int ascii = (int)character;
 
-        } else if (type == 3) {
+            if (type == 1) {
+                value = Integer.toBinaryString(ascii);
+            } else if (type == 2) {
+                value = Integer.toOctalString(ascii);
+            } else if (type == 3) {
+                value = Integer.toString(ascii);
+            } else if (type == 4) {
+                value = Integer.toHexString(ascii);
+            } else {
+                String s = String.valueOf(character);
+                value = s;
+            }
 
-        } else if (type == 4) {
+            if (num + o > decryptionLength - 2) {
+                o = -1;
+                num = 0;
+            }
 
-        } else {
+            System.out.print(value);
+
+            type++;
+            o++;
+
+            if (type <= 5) {
+            } else {
+                type = 1;
+            }
 
         }
 
